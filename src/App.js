b6621df6e8ed56fa41988b95eca97fa4e0c152b2
Quentin <q.lambkin@gmail.com>
2022-03-31 17:16:59 -0400

    Update domain

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import { Route, Routes } from 'react-router-dom';
+import AllMeetupsPage from './Pages/AllMeetups';
+import FavoritesPage from './Pages/Favorites';
+import NewMeetupsPage from './Pages/NewMeetups';
+import Layout from './Components/Layout/Layout';
+
+function App() {
+  return (
+    <Layout>
+      <Routes>
+        <Route path="/" element={<AllMeetupsPage />} />
+        <Route path="/new-meetup" element={<NewMeetupsPage />} />
+        <Route path="/favorites" element={<FavoritesPage />} />
+      </Routes>
+    </Layout>
+  );
+}
+
+export default App;
